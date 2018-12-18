58eba3fbf02fd32f77d0db4086237aad09f1febf
Marie Burns <mcburns2222@gmail.com>
2018-12-18 18:57:11 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -14,8 +14,8 @@ class Song
   def self.new_by_filename(f)
     filename = f.split (" - ")
     song = self.new(filename[1]) 
-    self.artist_name(filename[0])
     binding.pry
+    self.artist_name(filename[0])
   end
   
 end
